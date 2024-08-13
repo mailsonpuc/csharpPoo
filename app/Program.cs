907491572b8d3755e3b77234bd2c8ddb3488e512
mailsonpuc <mailson.costa@proton.me>
2024-08-12 23:49:47 -0300

    poo alunos

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using app.Models;
+
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "maria";
+p1.SobreNome = "silva";
+
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "ana";
+p2.SobreNome = "vasconcelo";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListaAlunos();
+
+//p1.Idade = 22;
+//p1.Apresentar();

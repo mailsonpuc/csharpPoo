f7d92ce6125c93a501c76147886928480d48bd9a
mailsonpuc <mailson.costa@proton.me>
2025-01-20 08:58:13 -0300

    Passando Nota,Idade para o construtor

--- Models/ex001/Aluno.cs
+++ Models/ex001/Aluno.cs
@@ -4,9 +4,10 @@ namespace csharpPoo.Models.ex001
     public class Aluno : Pessoa
     {
         //base(), passando o _nome de aluno para class pai Pessoa
-        public Aluno(string _nome) : base(_nome)
+        public Aluno(string _nome, double _nota, int _idade) : base(_nome)
         {
-
+            Nota = _nota;
+            Idade = _idade;
         }
 
 
--- Program.cs
+++ Program.cs
@@ -5,12 +5,10 @@ using csharpPoo.Models.ex002;
 Console.Clear();
 
 
-// Console.WriteLine("-".PadLeft(80, '-'));
+Console.WriteLine("-".PadLeft(80, '-'));
 
-// Aluno al1 = new Aluno("Mailson");
-// al1.Idade = 22;
-// al1.Nota = 10;
-// al1.Apresentar();
+Aluno al1 = new Aluno("Mailson", 10, 28);
+al1.Apresentar();
 
 
 // Console.WriteLine("-".PadLeft(80, '-'));
@@ -21,18 +19,18 @@ Console.Clear();
 // Console.WriteLine("-".PadLeft(80, '-'));
 
 
-Console.WriteLine("-".PadLeft(80, '-'));
+// Console.WriteLine("-".PadLeft(80, '-'));
 
-Soma sm1 = new Soma(5, 5);
-sm1.Resultado();
+// Soma sm1 = new Soma(5, 5);
+// sm1.Resultado();
 
-Subtrair sub1 = new Subtrair(10, 5);
-sub1.Resultado();
+// Subtrair sub1 = new Subtrair(10, 5);
+// sub1.Resultado();
 
-Multiplicar mult1 = new Multiplicar(5, 5);
-mult1.Resultado();
+// Multiplicar mult1 = new Multiplicar(5, 5);
+// mult1.Resultado();
 
-Dividir divi1 = new Dividir(100, 2);
-divi1.Resultado();
+// Dividir divi1 = new Dividir(100, 2);
+// divi1.Resultado();
 
-Console.WriteLine("-".PadLeft(80, '-'));
+// Console.WriteLine("-".PadLeft(80, '-'));
